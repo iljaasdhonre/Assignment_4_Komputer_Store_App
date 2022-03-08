f163e2c5e6c5df0d128405a6fc9cdf1960dbcede
iljaasd <iljaasd@gmail.com>
2022-03-08 16:21:29 +0100

    stored elements in variables

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
+console.log("hello");
 
+const bankBalanceElement = document.getElementById("bankBalance");
+const loanBalanceElement = document.getElementById("loanBalance");
+const loanButtonElement = document.getElementById("loan");
+const paymentBalanceElement = document.getElementById("paymentBalance");
+const bankButtonElement = document.getElementById("bank");
+const salaryButtonElement = document.getElementById("salary");
+const laptopsElement = document.getElementById("laptops");
+const specsElement = document.getElementById("specs");
+const imageElement = document.getElementById("image");
+const titleElement = document.getElementById("title");
+const descriptionElement = document.getElementById("description");
+const priceElement = document.getElementById("price");
+const buyButtonElement = document.getElementById("buy");
+
+let bankBalance = 0.0;
 

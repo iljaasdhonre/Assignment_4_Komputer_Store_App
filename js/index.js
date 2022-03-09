b0b617ff7f243cfaf3f09c40fb1a08ee64651186
iljaasd <iljaasd@gmail.com>
2022-03-09 12:46:39 +0100

    move salary to bank with outstanding loan in mind

--- js/index.js
+++ js/index.js
@@ -15,9 +15,11 @@ const priceElement = document.getElementById("price");
 const buyButtonElement = document.getElementById("buy");
 
 let paymentBalance = 0.0;
+let bankBalance = paymentBalance;
+let loanBalance = 0.0;
 let computers = [];
 
-//Fetch the computerdata
+//fetch the computerdata
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
 .then(response => response.json())
 .then(data => computers = data)
@@ -30,6 +32,7 @@ const addComputersToList = (computersData) => {
     computersData.forEach(x => addComputerToList(x));
 }
 
+//appending computertitles to the dropdownment
 const addComputerToList = (computer) => {
     const computerElement = document.createElement("option");
     computerElement.value = computer.id;
@@ -37,8 +40,37 @@ const addComputerToList = (computer) => {
     computersElement.appendChild(computerElement);
 }
 
-
+//update paymentBalance by 100 on each click
 function updateSalary(){
     paymentBalance += 100.00;
+    paymentBalanceElement.innerText = paymentBalance;
+}
+
+//update bankBalance by moving paymentBalance to bankBalance and setting paymentBalance to 0
+function moveSalaryToBank(){
+
+    //get 10% of salary for loan payment
+    const tenPercentOfSalaryToBank = (10 / 100) * paymentBalance;
+    let toBankBalance = paymentBalance - tenPercentOfSalaryToBank;
+    
+    if(loanBalance === 0.0){
+        bankBalance += paymentBalance;
+    }
+    else if(loanBalance > tenPercentOfSalaryToBank){
+        loanBalance -= tenPercentOfSalaryToBank
+        bankBalance += toBankBalance
+
+    }
+    else if(loanBalance < tenPercentOfSalaryToBank ){
+        loanBalance = 0.0;
+        toBankBalance += (tenPercentOfSalaryToBank - loanBalance);
+        bankBalance += toBankBalance;
+
+
+    }
+
+    paymentBalance = 0.0;
+    bankBalanceElement.innerText = bankBalance;
+    loanBalanceElement.innerText = loanBalance;
     paymentBalanceElement.innerText = paymentBalance;
 }

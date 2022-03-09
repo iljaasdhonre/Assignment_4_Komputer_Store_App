f17dbf1760888cf5b3d0ec3fd30e66739e66ebf0
iljaasd <iljaasd@gmail.com>
2022-03-09 11:13:15 +0100

    fetchin api data

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const loanButtonElement = document.getElementById("loan");
 const paymentBalanceElement = document.getElementById("paymentBalance");
 const bankButtonElement = document.getElementById("bank");
 const salaryButtonElement = document.getElementById("salary");
-const laptopsElement = document.getElementById("laptops");
+const computersElement = document.getElementById("computers");
 const specsElement = document.getElementById("specs");
 const imageElement = document.getElementById("image");
 const titleElement = document.getElementById("title");
@@ -15,4 +15,24 @@ const priceElement = document.getElementById("price");
 const buyButtonElement = document.getElementById("buy");
 
 let bankBalance = 0.0;
+let computers = [];
 
+//Fetch the computerdata
+fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+.then(response => response.json())
+.then(data => computers = data)
+.then(computersData => addComputersToList(computersData))
+.catch(function(err){
+    console.log(err);
+})
+
+const addComputersToList = (computersData) => {
+    computersData.forEach(x => addComputerToList(x));
+}
+
+const addComputerToList = (computer) => {
+    const computerElement = document.createElement("option");
+    computerElement.value = computer.id;
+    computerElement.appendChild(document.createTextNode(computer.title));
+    computersElement.appendChild(computerElement);
+}

4c5c0bf25649b76115f6ec80d9644f99634517d6
iljaasd <iljaasd@gmail.com>
2022-03-11 10:13:33 +0100

    user can buy a computer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const bankBalanceElement = document.getElementById("bankBalance");
 const loanBalanceElement = document.getElementById("loanBalance");
+const outstandingLoanElement = document.getElementById("outstandingLoan")
 const loanButtonElement = document.getElementById("loan");
 const paymentBalanceElement = document.getElementById("paymentBalance");
 const bankButtonElement = document.getElementById("bank");
@@ -16,8 +17,11 @@ let paymentBalance = 0.0;
 let bankBalance = paymentBalance;
 let loanBalance = 0.0;
 let computers = [];
+const outstandingLoanDiv = document.createElement("div");
+const outstandingLoanLabel = document.createElement("label");
+const outstandingLoanSpan = document.createElement("span");
 
-//fetch the computerdata
+//Fetch the computerdata
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
 .then(response => response.json())
 .then(data => computers = data)
@@ -26,6 +30,7 @@ fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
     console.log(err);
 })
 
+//Populate list with computers and update index.html with initial computer info
 const addComputersToList = (computersData) => {
     computersData.forEach(x => addComputerToList(x));
     specsElement.innerText = computersData[0].specs;
@@ -35,7 +40,7 @@ const addComputersToList = (computersData) => {
     imageElement.src = "https://noroff-komputer-store-api.herokuapp.com/" + computersData[0].image;
 }
 
-//appending computertitles to the dropdownment
+//Appending computertitles to the dropdownmenu
 const addComputerToList = (computer) => {
     const computerElement = document.createElement("option");
     computerElement.value = computer.id;
@@ -43,16 +48,16 @@ const addComputerToList = (computer) => {
     computersElement.appendChild(computerElement);
 }
 
-//update paymentBalance by 100 on each click
+//Update paymentBalance by 100 on each click
 const updateSalary = () => {
     paymentBalance += 100.00;
-    paymentBalanceElement.innerText = paymentBalance;
+    updateBalance(paymentBalanceElement, paymentBalance);
 };
 
-//update bankBalance by moving paymentBalance to bankBalance and setting paymentBalance to 0
+//Update bankBalance by moving paymentBalance to bankBalance and setting paymentBalance to 0
 function moveSalaryToBank(){
 
-    //get 10% of salary for loan payment
+    //Get 10% of salary for loan payment
     const tenPercentOfSalaryToBank = (10 / 100) * paymentBalance;
     let toBankBalance = paymentBalance - tenPercentOfSalaryToBank;
     
@@ -62,44 +67,101 @@ function moveSalaryToBank(){
     else if(loanBalance > tenPercentOfSalaryToBank){
         loanBalance -= tenPercentOfSalaryToBank
         bankBalance += toBankBalance
+        updateBalance(outstandingLoanSpan, loanBalance);
     }
-    else if(loanBalance < tenPercentOfSalaryToBank ){
+    else if(loanBalance <= tenPercentOfSalaryToBank ){
         loanBalance = 0.0;
+        updateDiv("remove");
         toBankBalance += (tenPercentOfSalaryToBank - loanBalance);
         bankBalance += toBankBalance;
     }
 
     paymentBalance = 0.0;
-    bankBalanceElement.innerText = bankBalance;
-    loanBalanceElement.innerText = loanBalance;
-    paymentBalanceElement.innerText = paymentBalance;
+    updateBalance(bankBalanceElement, bankBalance);
+    updateBalance(paymentBalanceElement, paymentBalance);
 }
 
 //Prompt user to insert the amount for a loan and check against the actual loanBalance and bankBalance
 function applyForLoan(){
 
         if(loanBalance != 0.0){
-            throwAlert("You already have a loan, you can not loan any more money.")   
+            throwAlert("You already have a loan, you cannot loan any more money.")   
         }
         else{
             const loanAmount = Number(window.prompt("How much would you like to loan?", "Amount"));
+            loanBalance = loanAmount;
             checkValidLoanAmount(loanAmount);
         }
 }
 
 //Check type of user input and against the actual loanBalance and bankBalance
-function checkValidLoanAmount(amount){
+function checkValidLoanAmount(loanAmount){
 
-    if(amount.isNan | typeof amount == 'string' | amount == null){
+    if(Number.isNaN(loanAmount) || loanAmount == 0){
         throwAlert("Please insert a valid amount")
     }
-    else if(amount/bankBalance > 2){
+    else if(loanAmount/bankBalance > 2){
         throwAlert("The amount you can loan cannot be more than double your bankaccount")
     }
     else{
-        loanBalance = amount
-        loanBalanceElement.innerText = amount
+        processLoanApplication(loanAmount);
     }
+}
+
+//Raise bankBalance with loanamount and create the outstanding loan label, amount and repay loan button.
+function processLoanApplication(loanAmount){
+    bankBalance += loanAmount;
+    updateBalance(bankBalanceElement, bankBalance);
+    updateDiv("build", loanAmount);
+
+}
+
+//Handle the onClick event for the buy now button
+function buyComputer(){
+    const laptopPrice = Number(priceElement.innerText);
+    const laptopTitle = titleElement.innerText;
+
+    if(!canBuyLaptop(laptopPrice)){
+        throwAlert("You cannot afford to buy this computer");
+    }
+    else{
+        bankBalanceElement.innerText = bankBalance;
+        throwAlert("You are now the proud owner of the " + laptopTitle);
+        
+    }
+}
+
+//Check if the price of the computer is lower than the bankBalance and set the bankBalance lower if so
+function canBuyLaptop(price){
+    if(price <= bankBalance){
+        bankBalance -= price;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//Format the given balance to currency and pass into HTMLelement
+const updateBalance = (element, balance) => {
+    const formattedBalance = new Intl.NumberFormat('nl-NL', { style: 'currency', currency: 'EUR' }).format(balance);
+    element.innerText = formattedBalance;
+}
+
+//Show or remove div element when loan is created or paid in full
+const updateDiv = (action, loanAmount) => {
+    if(action === "build"){
+        outstandingLoanLabel.innerText = "Outstanding loan ";
+        updateBalance(outstandingLoanSpan, loanAmount)
+        //outstandingLoanSpan.innerText = loanAmount;
+        outstandingLoanDiv.appendChild(outstandingLoanLabel);
+        outstandingLoanDiv.appendChild(outstandingLoanSpan);
+        outstandingLoanElement.appendChild(outstandingLoanDiv);
+    }else if(action === "remove"){
+        outstandingLoanDiv.removeChild(outstandingLoanSpan);
+        outstandingLoanDiv.removeChild(outstandingLoanLabel);
+        outstandingLoanDiv.remove();
+    }   
 }
 
 //Custom throw alert
@@ -107,7 +169,7 @@ function throwAlert(message){
     return alert(message)
 }
 
-//Show data of computer from dropdow
+//Show data of computer from dropdownmenu
 const handleComputerListChange = e => {
     const selectedComputer = computers[e.target.selectedIndex];
     specsElement.innerText = selectedComputer.specs;
@@ -118,6 +180,6 @@ const handleComputerListChange = e => {
     imageElement.alt = selectedComputer.title;
 }
 
+//Handle computer change in dropdownmenu
 computersElement.addEventListener('change', handleComputerListChange);
-
 
